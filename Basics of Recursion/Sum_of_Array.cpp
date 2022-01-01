e99906f52c47229a92ec42d7b64c586a2645ffee
anurag596savage <73139414+anurag596savage@users.noreply.github.com>
2022-01-01 07:20:15 +0530

    Create Sum_of_Array.cpp

---
+++ Basics of Recursion/Sum_of_Array.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int sum(int input[], int n) {
+  
+    if(n==0)
+    {
+        return 0;
+    }
+    int smallAns = sum(input+1,n-1);
+    return input[0]+smallAns;
+
+}
+
+int main(){
+    int n;
+    cin >> n;
+  
+    int *input = new int[n];
+    
+    for(int i = 0; i < n; i++) {
+        cin >> input[i];
+    }
+    
+    cout << sum(input, n) << endl;
+}
