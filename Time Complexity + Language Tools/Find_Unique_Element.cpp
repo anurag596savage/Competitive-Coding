385eb677bf68dcfd6d52672a4cf31c98f8787b7a
anurag596savage <73139414+anurag596savage@users.noreply.github.com>
2022-01-02 05:16:33 +0530

    Create Find_Unique_Element.cpp

---
+++ Time Complexity + Language Tools/Find_Unique_Element.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int findUnique(int *arr, int n) {
+    // Write your code here
+    int ans=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        ans=ans^arr[i];
+    }
+    return ans;
+}
+
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int size;
+        cin >> size;
+        int *input = new int[size];
+
+        for (int i = 0; i < size; ++i) {
+            cin >> input[i];
+        }
+
+        cout << findUnique(input, size) << endl;
+    }
+
+    return 0;
+}
