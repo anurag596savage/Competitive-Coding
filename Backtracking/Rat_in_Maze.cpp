9c398192a799feeec77d6134604f96cf4c8d0add
anurag596savage <73139414+anurag596savage@users.noreply.github.com>
2022-01-19 08:56:32 +0530

    Update Rat_in_Maze.cpp

--- Backtracking/Rat_in_Maze.cpp
+++ Backtracking/Rat_in_Maze.cpp
@@ -2,8 +2,17 @@
 
 You are given an integer N and a binary 2D array of size N*N. The given binary matrix represents a maze and a rat starts from the top left cell 
 and has to reach to the bottom right cell of the maze. The rat can move in all the four directions: up, down, left and right.
+
 In the given binary matrix, 0 signifies the cell is a dead end and 1 signifies the cell can be used to move from source to destination cell.
 You have to print all the paths, following which rat can move from the top left cell to the bottom right cell of the given binary matrix.
+
+Input Format ->
+The first line of input contains an integer, that denotes the value of N.
+Each of the following N lines denote rows of the binary matrix and contains either 0 or 1. Each row of the binary matrix contains N elements. 
+
+Output Format ->
+Every possible solution has to be printed on a separate line. Each of the possible solution will have N*N elements, printed row wise 
+and separated by space. The cells that are part of the path should be 1 and remaining all cells should be 0. Please refer to sample test cases for more clarity.   
 
 */
 
